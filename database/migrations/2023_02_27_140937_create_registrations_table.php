729454a8b738b68720ef044c121b0615fc386ce4
Paulo Alexandre Santos <tecnicopaulo@outlook.pt>
2025-12-04 23:13:34 +0100

    dsfv

--- database/migrations/2023_02_27_140937_create_registrations_table.php
+++ database/migrations/2023_02_27_140937_create_registrations_table.php
@@ -15,38 +15,21 @@ class CreateRegistrationsTable extends Migration
     {
         Schema::create('registrations', function (Blueprint $table) {
             $table->id();
+
+
             $table->string('code');
+            $table->string('plafond');
+            $table->string('nif');
+            $table->string('name');
+            $table->string('email');
+            $table->string('tel');
+            $table->string('quantity')->default(1);
+            $table->string('type');
+            $table->string('paymethod');
+            $table->string('payment_id')->nullable();
+            $table->enum('status', ['APROVADO', 'EMITIDO', 'RECEBIDO', 'DUPLICADO'])->default('RECEBIDO');
 
-            // Delegate information
-            $table->enum('type', ['Yes', 'No']); // registering on behalf
-            $table->enum('based', ['Yes', 'No']); // based in Angola
-            $table->string('nationality');
-            $table->enum('title', ['Dr', 'Miss', 'Mrs', 'Ms', 'Mr', 'Prof']);
-            $table->string('fullname');
-            $table->string('phone');
-            $table->string('email')->nullable();
-
-            // Organization details
-            $table->string('org_type');
-            $table->string('org_name');
-            $table->string('position');
-            // Head of delegation
-            $table->enum('head_of_delegation', ['Yes', 'No']);
-
-            // Accommodation & diet (só obrigatórios se based = No)
-            $table->enum('accommodation', ['Yes', 'No'])->nullable();
-            $table->string('diet')->nullable();
-
-            // Travel Information (só obrigatórios se based = No)
-            $table->string('passport_number')->nullable();
-            $table->string('passport_type')->nullable(); // Ordinary, Diplomatic, Service
-            $table->string('visa_status')->nullable();
-            $table->string('country_of_departure')->nullable();
-            $table->date('arrival_date')->nullable();
-            $table->date('departure_date')->nullable();
-
-            $table->string('level')->default('PARTICIPANT');
-            $table->enum('status', ['IMPRESSO', 'APROVADO', 'RECEBIDO', 'DUPLICADO'])->default('RECEBIDO');
+            $table->string('printed_at')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
