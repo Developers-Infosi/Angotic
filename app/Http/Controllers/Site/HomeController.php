66e8770ace46ebe101597e37026a231175c58bba
Paulo Alexandre Santos <tecnicopaulo@outlook.pt>
2025-12-05 16:13:45 +0100

    dsfd

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends Controller
         $response['schedulesII'] = Schedule::where('day', 'II')->orderby('start','asc')->get();
         $response['schedulesIII'] = Schedule::where('day', 'III')->orderby('start','asc')->get();
        
-        $response['speakers'] = Speaker::orderBy('priority', 'asc')->limit(12)->get();
+        $response['speakers'] = Speaker::orderBy('priority', 'desc')->limit(16)->get();
         return view('site.home.index', $response);
     }
 }
